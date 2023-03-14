96e40d78136e37a861900bc124a9ab0fdf825606
吴陈睿（Andy） <andy.wu@aishu.cn>
2023-03-14 11:04:01 +0800

    feat: setup

---
+++ tauri/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
+      <iframe src="https://anyshare.aishu.cn" style={{ height: 500 }}></iframe>
+    </div>
+  );
+}
+
+export default App;
